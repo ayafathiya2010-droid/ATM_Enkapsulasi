a90631c9d221a199e394e7deeaecae7ce9583868
ayafathiya2010-droid <ayafathiya2010@gmail.com>
2026-04-01 10:01:19 +0700

    Membuat set up awal class rekening

--- ATM_Enkapsulasi/Program.cs
+++ ATM_Enkapsulasi/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿class Rekening
+{
+    private string _noRekening;
+    private int _saldo;
+
+    public string NoRekening
+    {
+        set { _noRekening = value; }
+    }
+
+    public int  Saldo
+    {
+        get { return _saldo; }
+        set
+        {
+            if (value < 0) Console.WriteLine( "Saldo tidak boleh negatif!!");
+            else _saldo = value;
+        }
+    }
+
+    public Rekening (string no_rekening)
+    {
+        NoRekening = no_rekening;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Menampilkan info...");
+        Console.WriteLine($"Saldo saat ini: {Saldo}");
+    }
+}
